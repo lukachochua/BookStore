0c6d226d30cd8956b30a0e18f023f29578598b2d
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-07-19 14:40:53 +0400

    feat: create first model

---
+++ BookStoreWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
