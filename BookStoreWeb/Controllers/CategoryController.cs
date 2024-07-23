9786a020235be215ff8ca0f2e9ee6eb2d17dfb54
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-07-23 14:25:46 +0400

    feat: retrieve all categories

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult index()
         {
+            var objCategoryList = _db.Categories.ToList(); 
             return View();
         }
     }
