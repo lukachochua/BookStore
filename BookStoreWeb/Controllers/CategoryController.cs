6d8aff8895e24c716b40647e221a3909d368ba4e
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-07-24 12:12:03 +0400

    feat: edit category get functionality

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -11,14 +11,14 @@ namespace BookStoreWeb.Controllers
         {
             _db = db;
         }
-        public IActionResult index()
+        public IActionResult Index()
         {
-            IEnumerable<Category> objCategoryList = _db.Categories.ToList(); 
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
 
         //GET
-        public IActionResult create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -40,7 +40,46 @@ namespace BookStoreWeb.Controllers
                 return RedirectToAction("Index");
             }
 
-            return View(category); 
+            return View(category);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            /*            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.id==id);
+                        var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.id == id);*/
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
         }
     }
 }
